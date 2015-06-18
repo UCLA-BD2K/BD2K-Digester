3e382ef706dae4c72259405b7bd171e4a6b18595
Alan Kha <akhahaha@gmail.com>
2015-06-18 16:49:01 -0700

    Implemented Diff File Output

--- src/Digester.java
+++ src/Digester.java
@@ -1,4 +1,6 @@
 import crawlers.*;
+import difflib.Delta;
+import difflib.Patch;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -9,6 +11,9 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static java.lang.System.exit;
 
@@ -16,13 +21,28 @@ import static java.lang.System.exit;
  * Created by Alan on 6/15/2015.
  */
 public class Digester {
-    private final static String DEFAULT_FILETYPE_FILTERS = ".*(\\.(css|gif|js|jpg|png|mp3|mp3|zip|gz))";
+    private final static String DEFAULT_FILETYPE_FILTERS = ".*(\\.(css|gif|js|jpg|png|mp3|mp3|zip|gz|ico))";
 
-    public static void main(String[] args) throws Exception {
-        runFromXML("siteData.xml");
+    private final static String DIFF_OUTPUT_FOLDER = "data/diffs/";
+    private final static DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+    private static BufferedWriter writer;
+
+    public static void main(String[] args) {
+        try {
+            // Create intermediate directories if necessary
+            new File(DIFF_OUTPUT_FOLDER).mkdirs();
+            // Create new timestamped file
+            writer = new BufferedWriter(new FileWriter(
+                    new File(DIFF_OUTPUT_FOLDER + "DIFF" + "_" + dateFormat.format(new Date()) + ".txt")));
+            runFromXML("siteData.xml", "UCLA");
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void runFromXML(String filename) {
+    public static void runFromXML(String filename, String runOnlyID) throws IOException {
+        // Open and run sites from XML file
         File fXmlFile = new File(filename);
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = null;
@@ -41,6 +61,7 @@ public class Digester {
             exit(-1);
         }
 
+        // Iterate through crawl nodes
         doc.getDocumentElement().normalize();
         NodeList crawlNodes = doc.getElementsByTagName("CrawlID");
         for (int temp = 0; temp < crawlNodes.getLength(); temp++) {
@@ -50,6 +71,12 @@ public class Digester {
                 String crawlID = crawlNode.getAttribute("ID");
                 String RootURL = crawlNode.getElementsByTagName("RootURL").item(0).getTextContent();
 
+                // Skip if not run only
+                if (runOnlyID != null && !runOnlyID.equals(crawlID)) {
+                    continue;
+                }
+
+                // Process seed URLs
                 NodeList seedNodes = crawlNode.getElementsByTagName("SeedURLs");
                 int numSeeds = seedNodes.getLength();
                 String seedList[] = new String[numSeeds];
@@ -57,9 +84,9 @@ public class Digester {
                     seedList[i] = seedNodes.item(i).getTextContent();
                 }
 
+                // Process URL excludes
                 NodeList excludeNodes = crawlNode.getElementsByTagName("ExcludeURLs");
                 int numExcludes = excludeNodes.getLength();
-                ;
                 String excludeList[] = new String[numExcludes];
                 for (int i = 0; i < numExcludes; i++) {
                     excludeList[i] = excludeNodes.item(i).getTextContent();
@@ -67,10 +94,29 @@ public class Digester {
 
                 String specialTextPattern = crawlNode.getElementsByTagName("SpecialText").item(0).getTextContent();
 
+                // Create and run crawler
                 DigestCrawler crawler = new DigestCrawler(crawlID, RootURL, seedList,
                         DEFAULT_FILETYPE_FILTERS, excludeList, specialTextPattern);
-                crawler.digest();
+                Patch<String> patch = crawler.digest();
+
+                // Output results to diff file
+                writer.write(crawler.getCrawlID() + "\n");
+                writer.newLine();
+                if (patch == null) {
+                    writer.write("No changes" + "\n");
+                } else {
+                    patch.getDeltas().forEach((Delta delta) -> {
+                        try {
+                            writer.write(delta.toString() + "\n");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                }
+                writer.newLine();
             }
         }
+
+        writer.close();
     }
 }
